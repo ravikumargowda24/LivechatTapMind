b02815849c827dd3ca91f717e17e298f5e530e46
ravikumargowda24 <ravikumar.gs@gmail.com>
2025-10-05 21:04:37 +0530

    first commit

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -10,22 +10,22 @@ import {
   getUserStatus,
 } from "../controllers/AuthController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-import multer from "multer";
+// import multer from "multer";
 
 const authRoutes = Router();
-const upload = multer({ dest: "uploads/profiles/" });
+// const upload = multer({ dest: "uploads/profiles/" });
 
 authRoutes.post("/signup", signup);
 authRoutes.post("/login", login);
 authRoutes.post("/logout", logout);
 authRoutes.get("/userinfo", verifyToken, getUserInfo);
 authRoutes.post("/update-profile", verifyToken, updateProfile);
-authRoutes.post(
-  "/add-profile-image",
-  verifyToken,
-  upload.single("profile-image"),
-  addProfileImage
-);
+// authRoutes.post(
+//   "/add-profile-image",
+//   verifyToken,
+//   upload.single("profile-image"),
+//   addProfileImage
+// );
 authRoutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
 authRoutes.get("/user-status/:userId", verifyToken, getUserStatus);
 
